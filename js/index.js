b85ae6bd5f22334e3bd50892f67593b224d59135
hmarnghitr <corecentric@gmail.com>
2015-05-28 21:36:06 -0400

    current state
    Most of my html5, css, some javaScript done. I still have a lot of work
    to do

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function(){
+//THis is the scrolling 	
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+//Isotope code 
+
+var $container = $('.isotope').isotope({
+    itemSelector: '.port-item',
+    layoutMode: 'fitRows',
+  });
+
+ // bind filter button click
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
+  
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+//tab code
+
+  $('ul.tabs li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current');
+  })
+
+
+});
